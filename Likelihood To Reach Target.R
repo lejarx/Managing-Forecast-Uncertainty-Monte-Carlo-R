846f98f6da3c872b43e155166993cdb484f48a09
Tristan Ganry <tristanganry@gmail.com>
2018-04-24 20:57:09 -0400

    Update Likelihood To Reach Target.R

--- Likelihood To Reach Target.R
+++ Likelihood To Reach Target.R
@@ -1,3 +1,4 @@
+#Copyright © 2018 Tristan Ganry
 library(RCurl)
 # Read data from Github - Qty sold over 15 years and in millions
 mydata <- read.csv(text = getURL("https://raw.githubusercontent.com/tristanga/MonteCarlo_ForecastRisk/master/TS.csv"))
